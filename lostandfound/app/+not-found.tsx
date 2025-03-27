9da36de965d94c808f7cf5f9caf2a6ca7bc87cc2
Shrijan Poudel <85357750+SlytherSavior@users.noreply.github.com>
2025-03-27 08:47:52 +0545

    usual firebase config and react native async storage

---
+++ lostandfound/app/+not-found.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text } from "react-native";
+import { Stack } from "expo-router";
+
+export default function NotFoundScreen() {
+    <>
+        <View>
+            <Text>Oops, this is not the screen you are lookin for</Text>
+        </View>
+    </>
+}
