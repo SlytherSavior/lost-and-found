3e44c2e383a060b68a7ce65ced848969d1689ca9
Shrijan Poudel <85357750+SlytherSavior@users.noreply.github.com>
2025-03-27 08:29:38 +0545

    feat : auth and directory fix

--- lostandfound/app/index.tsx
+++ lostandfound/app/index.tsx
@@ -1,14 +1,40 @@
 import { View, SafeAreaView, Text } from 'react-native';
 import React, { useState } from 'react';
+import { ThemedText } from '@/components/ThemedText';
 import { auth } from '../FirebaseConfig';
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from '@firebase/auth';
+import { router } from 'expo-router';
 
 const index = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+
+    const signIn = async () => {
+        try {
+            const user = await signInWithEmailAndPassword(auth, email, password);
+            if (user) router.replace('/(tabs)');
+        } catch (error: any) {
+            console.log(error)
+            alert('Sign in failed: ' + error.message);
+        }
+    }
+    const signUp = async () => {
+        try {
+            const user = await createUserWithEmailAndPassword(auth, email, password);
+            if (user) router.replace('/(tabs)');
+
+        } catch (error: any) {
+            console.log(error)
+            alert('Sign in failed: ' + error.message);
+        }
+    }
+
+
     return (
         <SafeAreaView>
-            <Text>dfafasd</Text>
+            <ThemedText>
+                Login Page
+            </ThemedText>
         </SafeAreaView>
     )
 }
