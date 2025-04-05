bbe6c05ce27b8468665e345f53ce6b40e57cd287
Shrijan Poudel <85357750+SlytherSavior@users.noreply.github.com>
2025-04-05 17:15:11 +0545

    remaining info

--- lostandfound/components/PubRecord.jsx
+++ lostandfound/components/PubRecord.jsx
@@ -5,16 +5,11 @@ const PubRecord = (record) => {
     //need to now display that record in a good component format style .... yeahhh
     /* soo we need the record 
     soo , for the record, the , hmmm first shall we integrate the data base ???? butt then validationnnn soo , at first let's integrate firebase i guess well fuck it we install firebase
-
-
-
-
-
     */
     return (
         <View>
-            <Text>PubRecord</Text>
-        </View>
+            <Text className="font-bold">PubRecord</Text>
+        </View >
     )
 }
 
