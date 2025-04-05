76397e41e462cacfbcf081d1832efda57a3e7dba
Shrijan Poudel <85357750+SlytherSavior@users.noreply.github.com>
2025-04-05 15:40:46 +0545

    setup firebase

--- lostandfound/firebaseConfig.js
+++ lostandfound/firebaseConfig.js
@@ -1,7 +1,12 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getFirestore, doc, getDoc } from "firebase/firestore"; //you have option for firebase/firestore/lite
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
+
+
+
+
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -13,5 +18,20 @@ const firebaseConfig = {
     appId: "1:745297315548:web:e2fc31d4c288ada5951619"
 };
 
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+async function readData() {
+    const docRef = doc(db, "Data", "g8wZ4B6IPg9m3L0aw8cS");
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+        console.log("Document data:", docSnap.data());
+    } else {
+        console.log("No such document!");
+    }
+}
+
+readData(); //fixing firestore , so apprently it is working
