d2f9edca5bd89b0443d5da5fc3e2e952b5a0a957
Shrijan Poudel <85357750+SlytherSavior@users.noreply.github.com>
2025-04-05 13:32:32 +0545

    icons changing interesting

--- lostandfound/app/(tabs)/_layout.tsx
+++ lostandfound/app/(tabs)/_layout.tsx
@@ -1,16 +1,29 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { Tabs } from 'expo-router'
+import AntDesign from '@expo/vector-icons/AntDesign';
+import Entypo from '@expo/vector-icons/Entypo';
+
+
+
 
 const _layout = () => {
     return (
         <Tabs>
             <Tabs.Screen
                 name="Home"
-                options={{ headerShown: false }} />
+                options={{
+                    headerShown: false,
+                    title: "Home",
+                    tabBarIcon: () => <AntDesign name="home" size={24} color="black" />,
+                }} />
             <Tabs.Screen
                 name="Guidelines"
-                options={{ headerShown: false }} />
+                options={{
+                    headerShown: false,
+                    title: "Guidelines",
+                    tabBarIcon: () => <Entypo name="text-document" size={24} color="black" />,
+                }} />
             <Tabs.Screen
                 name="New Request"
                 options={{ headerShown: false }} />
