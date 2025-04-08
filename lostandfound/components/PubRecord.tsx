bb6dddf508a1ff5ca6ddb5bd139ba6d825ea4e4f
Shrijan Poudel <85357750+SlytherSavior@users.noreply.github.com>
2025-04-08 11:13:43 +0545

    feat: conver jsx to tsx, add data.js exception

--- lostandfound/components/PubRecord.tsx
+++ lostandfound/components/PubRecord.tsx
@@ -1,7 +1,7 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-export function PubRecord(record) {
+export function PubRecord<T>(record: T) {
     //need to now display that record in a good component format style .... yeahhh
     /* soo we need the record 
     soo , for the record, the , hmmm first shall we integrate the data base ???? butt then validationnnn soo , at first let's integrate firebase i guess well fuck it we install firebase
