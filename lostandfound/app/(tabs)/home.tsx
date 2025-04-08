1e4bfb7ce437e9415ad5a2bff19667ad698c7892
Shrijan Poudel <85357750+SlytherSavior@users.noreply.github.com>
2025-04-09 05:28:18 +0545

    render in home working on it

--- lostandfound/app/(tabs)/home.tsx
+++ lostandfound/app/(tabs)/home.tsx
@@ -1,10 +1,17 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
-import { db } from '@/firebaseConfig';
+import { dataList } from '@/firebaseConfig';
 import { PubRecord } from '@/components/PubRecord';
 
 const Home = () => {
+    const fetchData = async () => {
+        const globalList = await dataList();
+        return globalList;
+    }
+    const mainList = fetchData();
+
+
     return (
         <SafeAreaView className="flex-1 justify-center items-center bg-background">
             <Text className="text-2xl text-primary">home</Text>
