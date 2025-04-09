e784ae8ade414fc4ad01c76019cd30e2aca77f74
Shrijan Poudel <85357750+SlytherSavior@users.noreply.github.com>
2025-04-10 05:19:18 +0545

    feat: card component

--- lostandfound/components/PubRecord.tsx
+++ lostandfound/components/PubRecord.tsx
@@ -1,7 +1,7 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-interface PubRecordProps {
+type PubRecordProps = {
     Category: string;
     Uploader: string;
     Status: boolean;
@@ -12,10 +12,33 @@ interface PubRecordProps {
 export function PubRecord(record: PubRecordProps, index: number) {
 
     return (
-        <View>
-            <Text className="font-bold">PubRecord</Text>
+        <View className="bg-card border-solid text-cardText flex flex-col m-2" style={styles.cardHolder} key={index}>
+            <View className="grid grid-cols-3 gap-2 " style={styles.titleBar}>
+                <Text className="font-bold  text-xl col-span-2">{record.Title}</Text>
+                <Text >{record.Category}</Text>
+            </View>
+            <View>
+                <Text className="text-lg" style={styles.Description}>{record.Description}</Text>
+            </View>
+            <View style={styles.Bottom} className="text-sm">
+                <Text>{record.Uploader}</Text>
+            </View>
+
         </View>
     )
 };
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    cardHolder: {
+        borderRadius: 50,
+    },
+    titleBar: {
+
+    },
+    Description: {
+
+    },
+    Bottom: {
+
+    }
+})
