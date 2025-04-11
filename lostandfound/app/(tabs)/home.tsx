adfda524331be8d35cf31976d63c6021c0c7372e
Shrijan Poudel <85357750+SlytherSavior@users.noreply.github.com>
2025-04-12 05:24:23 +0545

    fix: data filtering

--- lostandfound/app/(tabs)/home.tsx
+++ lostandfound/app/(tabs)/home.tsx
@@ -1,11 +1,10 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, ScrollView } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { db } from '@/firebaseConfig';
 import { PubRecord } from '@/components/PubRecord';
 import { collection, getDocs } from 'firebase/firestore';
-import { recordType } from '@/types';
-import { ScrollView } from 'react-native-reanimated/lib/typescript/Animated';
+import { recordType } from '@/types'
 
 const Home = () => {
     const collecData = collection(db, "Data");
@@ -15,21 +14,22 @@ const Home = () => {
             const dataList: recordType[] = response.docs.map((doc) => ({
                 id: doc.id,
                 data: doc.data(),
-            }))
+            })).filter((data) => {
+                return data.data.Status !== true;
+            })
             setMainList(dataList);
         }).catch(error => {
             console.log(error.message)
         })
-    }, [])
-
+    }, []);
 
 
     return (
         <SafeAreaView className="flex-1 justify-center items-center bg-background">
             <Text className="text-2xl text-prima ry">Home</Text>
-            <ScrollView >
+            <ScrollView className="bg-black p-4" >
                 {mainList.map((doc) => (
-                    <PubRecord record={doc}></PubRecord>
+                    <PubRecord record={doc} key={doc.id}></PubRecord>
                 ))}
             </ScrollView>
         </SafeAreaView>
