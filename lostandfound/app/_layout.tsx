6a441e0be673839b6e3653fdff9dd34ae06ccbaa
Shrijan Poudel <85357750+SlytherSavior@users.noreply.github.com>
2025-04-15 05:34:11 +0545

    make routes for login and signup

--- lostandfound/app/_layout.tsx
+++ lostandfound/app/_layout.tsx
@@ -5,6 +5,9 @@ export default function RootLayout() {
     <Stack.Screen
       name="(tabs)"
       options={{ headerShown: false }} />
+    <Stack.Screen
+      name="login"
+      options={{ headerShown: false }} />
     <Stack.Screen name="index" options={{ headerShown: false }} />
   </Stack>;
 }
