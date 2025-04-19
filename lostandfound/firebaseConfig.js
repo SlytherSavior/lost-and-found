88ebc5e8e5ac4c61775a864ae6cc5fce7383df6e
Shrijan Poudel <85357750+SlytherSavior@users.noreply.github.com>
2025-04-19 18:05:05 +0545

    not necessary but yeah

--- lostandfound/firebaseConfig.js
+++ lostandfound/firebaseConfig.js
@@ -7,12 +7,12 @@ import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDhv0mq3CZcRcJRvnt93LhdI6XomLxpEuk",
-    authDomain: "lost-found-b2d66.firebaseapp.com",
-    projectId: "lost-found-b2d66",
-    storageBucket: "lost-found-b2d66.firebasestorage.app",
-    messagingSenderId: "745297315548",
-    appId: "1:745297315548:web:e2fc31d4c288ada5951619"
+    apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
 };
 
 
