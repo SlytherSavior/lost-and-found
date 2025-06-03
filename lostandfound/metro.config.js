ee20e995ef9170bcb8d51d6edf80d02423b3dbdb
Shrijan Poudel <85357750+SlytherSavior@users.noreply.github.com>
2025-06-03 12:22:57 +0545

    updated config

--- lostandfound/metro.config.js
+++ lostandfound/metro.config.js
@@ -1,9 +1,13 @@
-const { getDefaultConfig } = require("expo/metro-config");
-const { withNativeWind } = require('nativewind/metro');
+const { getDefaultConfig } = require("@expo/metro-config");
+const { withNativeWind } = require("nativewind/metro");
 
 const config = getDefaultConfig(__dirname);
-config.resolver.sourceExts.push('cjs');
-config.resolver.unstable_enablePackageExports = false;
 
-module.exports = withNativeWind(config, { input: './app/globals.css' });
+const updatedConfig = withNativeWind(config, {
+  input: "./app/globals.css",
+});
 
+updatedConfig.resolver.sourceExts.push("cjs");
+updatedConfig.resolver.unstable_enablePackageExports = false;
+
+module.exports = updatedConfig;
